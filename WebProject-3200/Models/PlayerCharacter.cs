3b65e4385e13eaf00ba6e2a76f089f2681efb586
LEO <141844230+M-LEO-H@users.noreply.github.com>
2025-10-01 07:08:17 +0200

    Add story and user model classes
    Introduced model classes for Character, Choice, Dialogue, PlayerCharacter, StoryNode, and User to support story structure and user management. These models will be used for representing story nodes, dialogues, choices, characters, and users in the application.

---
+++ WebProject-3200/Models/PlayerCharacter.cs
@@ -0,0 +1,13 @@
+
+namespace WebProject_3200.Models;
+/// <summary>
+/// Represents a player character in the story, inheriting from Character.
+/// </summary>
+public class PlayerCharacter : Character
+{
+    /// <summary>
+    /// The health points of the player character.
+    /// </summary>
+    public int Health { get; set; } = 100;
+
+}
